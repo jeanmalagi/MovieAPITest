3ec67d720ba0b2d30e013c91ed65656aeb506ff3
idd_jmalagi <idd_jmalagi@uolinc.com>
2024-01-22 13:51:31 -0300

    Inclui regra do cenário de teste

--- src/test/java/MovieAPITest.java
+++ src/test/java/MovieAPITest.java
@@ -5,14 +5,29 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Assert;
 
+import static org.junit.Assert.assertEquals;
+
 public class MovieAPITest {
 
     private Response response;
+    private static final String AUTH_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjNlZDgzYjYyOThhYTljNTYyY2Q0MWE3NGIyNTMzZSIsInN1YiI6IjY1YTY3ODE5OTg4YWZkMDEyNTg5OWMxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F7K0QKEW5SU9PUOz2qH7dRw20lnJl6xolqYnQ9a466g\n" +
+            "\n";
 
-    @Given("a movie with ID {int}")
-    public void givenAMovieWithID(int movieId) {
-        // Configuração do cenário
-        RestAssured.baseURI = "https://api.themoviedb.org/3";
+    @Given("the user has a valid API token")
+    public void theUserHasAValidApiToken() {
+
+        Response response = RestAssured.given()
+                .baseUri("https://api.themoviedb.org/3")
+                .basePath("/authentication")
+                .header("accept", "application/json")
+                .header("Authorization", "Bearer " + AUTH_TOKEN)
+                .get();
+
+        // Extraindo o token do corpo da resposta
+        int statusCode = response.getStatusCode();
+        System.out.println("Código de Resposta: " + statusCode);
+
+        assertEquals(200, statusCode);
     }
 
     @When("I mark the movie as favorite")
@@ -27,10 +42,25 @@ public class MovieAPITest {
                 .post();
     }
 
+    @When("the user requests their favorite movies")
+    public void the_user_requests_their_favorite_movies() {
+
+        response = RestAssured.given()
+                .baseUri("https://api.themoviedb.org/3")
+                .basePath("/account/20926905/favorite/movies")
+                .queryParam("language", "en-US")
+                .queryParam("page", 1)
+                .queryParam("sort_by", "created_at.asc")
+                .header("accept", "application/json")
+                .header("Authorization", "Bearer " + AUTH_TOKEN)
+                .get();
+    }
+
+
     @Then("the response status code should be {int}")
     public void thenTheResponseStatusCodeShouldBe(int expectedStatusCode) {
         // Verificação do código de resposta
         int actualStatusCode = response.getStatusCode();
-        Assert.assertEquals(expectedStatusCode, actualStatusCode);
+        assertEquals(expectedStatusCode, actualStatusCode);
     }
 }
